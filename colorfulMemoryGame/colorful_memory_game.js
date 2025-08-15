133530e917e28f51743e82c7ef4e6748c157df3d
HASSAN-HKH <hassankhalaf409@gmail.com>
2025-08-15 09:45:15 -0400

    Game Updated

--- colorfulMemoryGame/colorful_memory_game.js
+++ colorfulMemoryGame/colorful_memory_game.js
@@ -4,7 +4,7 @@ let gameContainer = document.getElementById("gameContainer");
 let score = document.getElementById("score");
 let timer = document.getElementById("timer");
 let startbtn = document.getElementById("startbtn");
-
+ 
 const colors = [
   "red",
   "blue",
@@ -19,82 +19,166 @@ const colors = [
   "orange",
   "pink",
 ];
-let cards = shuffle(colors.concat(colors));
+
 let selectedCards = [];
 let userScore = 0;
-let timeLeft = 60;
-let gameInterval;
+let timeLeft = 30;
+let cards = shuffle(colors.concat(colors));
 
-function generateCards(){
-    for (let i = 0; i < cards.length; i++) {
-        let card = document.createElement("div");
-        card.innerHTML = "?";
-        // console.log(card);
-        card.setAttribute("class" , "card");
-        card.onclick = function(){
-            if (selectedCards.includes(card) || selectedCards.length === 2 || card.style.backgroundColor === "green") return;
-            card.innerHTML = cards[i];
-            card.style.backgroundColor = cards[i];
-            // console.log("Ok");
-            selectedCards.push(card);
-
-            if(selectedCards.length === 2){
-                setTimeout(function(){
-                    if(selectedCards[0].innerHTML === selectedCards[1].innerHTML){
-                        userScore++;
-                        score.innerHTML = `Score: ${userScore}`
-                        selectedCards[0].style.color = "white";
-                        selectedCards[0].style.backgroundColor = "green";
-                        selectedCards[1].style.color = "white";
-                        selectedCards[1].style.backgroundColor = "green";
-                        selectedCards.pop();
-                        selectedCards.pop();
-                    }
-                    else if(selectedCards[0].innerHTML !== selectedCards[1].innerHTML){
-                        selectedCards[0].innerHTML = "?";
-                        selectedCards[0].style.backgroundColor = "#ddd";
-                        selectedCards[1].innerHTML = "?";
-                        selectedCards[1].style.backgroundColor = "#ddd";
-                        selectedCards.pop();
-                        selectedCards.pop();
-                    }
-                } , 500)
-            }   
-        }
-        gameContainer.appendChild(card);
-    }
-}
 
 function shuffle(arr){
-    for (let i = arr.length; i > 0; i--) {
-        let randIndex = Math.floor(Math.random() * i);
-        [arr[i-1] , arr[randIndex]] = [arr[randIndex] , arr[i-1]];
+    for (let index = arr.length - 1; index >= 0; index--) {
+        let rand = Math.floor(Math.random() * (index + 1));
+        [arr[index] , arr[rand]] = [arr[rand] , arr[index]];
     }
     return arr;
 }
 
-// let start = false;
-startbtn.onclick = function(){
-    if(timeLeft === 60){
-        generateCards();
-        let time = setInterval(() => {
-            timer.innerHTML = `Time Left: ${timeLeft}`;
-            timeLeft--;
-            if(timeLeft === -1 || userScore === 12){
-                clearInterval(time);
-                if(userScore === 12){
-                    alert("You solved the game. Congrats");
-                }
-                else{
-                    alert("Game over, try again");
-                }    
-            }
-        }, 1000);
-    }
-    else{
-        window.location.reload();
+function generateCards(cards){
+    for (let index = 0; index < cards.length; index++) {
+        let card = document.createElement("div");
+        card.dataset.color = cards[index];
+        card.classList.add("card");
+        card.textContent = "?";
+        gameContainer.appendChild(card);
     }
 }
+
+function cardEvent(event){
+    let card = event.target;
+    if(!card.classList.contains("card") || card.classList.contains("matched")){
+        return;
+    }
+    card.style.backgroundColor = card.dataset.color;
+    card.textContent = card.dataset.color;
+    selectedCards.push(card);
+    if(selectedCards.length === 2){
+        checkMatch();
+    }
+}
+
+function checkMatch(){
+    const [card1 , card2] = selectedCards;
+    setTimeout(function(){
+        if(card1.dataset.color === card2.dataset.color){
+            card1.classList.add("matched");
+            card2.classList.add("matched");
+            userScore += 1;
+            score.textContent = `Score: ${userScore}`;
+        }
+        else{
+            card1.textContent = "?";
+            card2.textContent = "?";
+            card1.style.backgroundColor = "#ddd";
+            card2.style.backgroundColor = "#ddd";
+        }
+    } , 500);
+    selectedCards = [];
+
+}
+
+// function startGame(){
+    
+// }
+
+startbtn.onclick = function(){
+    startbtn.disabled = true;
+    startGameTimer(timeLeft);
+    generateCards(cards);
+    gameContainer.addEventListener("click" , cardEvent);
+}
+
+function startGameTimer(timeLeft){
+    timer.textContent = `Time Left: ${timeLeft}`;
+    let gameInterval = setInterval(() => {
+        timeLeft--;
+        timer.textContent = `Time Left: ${timeLeft}`;
+        if (timeLeft === 0) {
+            clearInterval(gameInterval);
+            let timeLeft = 30;
+            alert('Game Over!');
+            startbtn.disabled = false;
+        }
+    }, 1000);
+}
+
+
+// let cards = shuffle(colors.concat(colors));
+// let selectedCards = [];
+// let userScore = 0;
+// let timeLeft = 60;
+// let gameInterval;
+
+// function generateCards(){
+//     for (let i = 0; i < cards.length; i++) {
+//         let card = document.createElement("div");
+//         card.innerHTML = "?";
+//         // console.log(card);
+//         card.setAttribute("class" , "card");
+//         card.onclick = function(){
+//             if (selectedCards.includes(card) || selectedCards.length === 2 || card.style.backgroundColor === "green") return;
+//             card.innerHTML = cards[i];
+//             card.style.backgroundColor = cards[i];
+//             // console.log("Ok");
+//             selectedCards.push(card);
+
+//             if(selectedCards.length === 2){
+//                 setTimeout(function(){
+//                     if(selectedCards[0].innerHTML === selectedCards[1].innerHTML){
+//                         userScore++;
+//                         score.innerHTML = `Score: ${userScore}`
+//                         selectedCards[0].style.color = "white";
+//                         selectedCards[0].style.backgroundColor = "green";
+//                         selectedCards[1].style.color = "white";
+//                         selectedCards[1].style.backgroundColor = "green";
+//                         selectedCards.pop();
+//                         selectedCards.pop();
+//                     }
+//                     else if(selectedCards[0].innerHTML !== selectedCards[1].innerHTML){
+//                         selectedCards[0].innerHTML = "?";
+//                         selectedCards[0].style.backgroundColor = "#ddd";
+//                         selectedCards[1].innerHTML = "?";
+//                         selectedCards[1].style.backgroundColor = "#ddd";
+//                         selectedCards.pop();
+//                         selectedCards.pop();
+//                     }
+//                 } , 500)
+//             }   
+//         }
+//         gameContainer.appendChild(card);
+//     }
+// }
+
+// function shuffle(arr){
+//     for (let i = arr.length; i > 0; i--) {
+//         let randIndex = Math.floor(Math.random() * i);
+//         [arr[i-1] , arr[randIndex]] = [arr[randIndex] , arr[i-1]];
+//     }
+//     return arr;
+// }
+
+// // let start = false;
+// startbtn.onclick = function(){
+//     if(timeLeft === 60){
+//         generateCards();
+//         let time = setInterval(() => {
+//             timer.innerHTML = `Time Left: ${timeLeft}`;
+//             timeLeft--;
+//             if(timeLeft === -1 || userScore === 12){
+//                 clearInterval(time);
+//                 if(userScore === 12){
+//                     alert("You solved the game. Congrats");
+//                 }
+//                 else{
+//                     alert("Game over, try again");
+//                 }    
+//             }
+//         }, 1000);
+//     }
+//     else{
+//         window.location.reload();
+//     }
+// }
 
 
 
